da7399dc5fa37e294acb9434d5239256cf4426ca
minjae-lulu <lmjbsj@unist.ac.kr>
2022-07-13 10:43:36 +0900

    update

---
+++ code/2_week_aloam/aloam-tutorial/cakin_aloam/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/aloam-tutorial/cakin_aloam/src
+# Build directory: /home/user/Desktop/aloam-tutorial/cakin_aloam/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("A-LOAM")
---
+++ code/2_week_aloam/aloam-tutorial/cakin_pub/build/file_player_mulran/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/aloam-tutorial/cakin_pub/src/file_player_mulran
+# Build directory: /home/user/Desktop/aloam-tutorial/cakin_pub/build/file_player_mulran
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
