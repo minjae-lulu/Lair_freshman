4fe1859f6d61ee3b9079873bf2c784e115eafc0e
minjae-lulu <lmjbsj@unist.ac.kr>
2022-07-21 20:32:16 +0900

    update

---
+++ code/3_week_datasegmentation/catkin_ws/build/segmentation_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/catkin_ws/src/segmentation_ros
+# Build directory: /home/user/Desktop/catkin_ws/build/segmentation_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ md_file/code/2_week_aloam/aloam-tutorial/cakin_pub/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/aloam-tutorial/cakin_pub/src
+# Build directory: /home/user/Desktop/aloam-tutorial/cakin_pub/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("file_player_mulran")
